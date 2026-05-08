7ff2847151c37b8a2a74c809a04feca05b93da57
Hatim Malak <hatim05042006@gmail.com>
2026-05-08 18:44:14 +0530

    Add allowed origin for CORS in FastAPI application

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://hatim-porfolio.vercel.app"
 ]
 
 # Add the CORS middleware to your FastAPI application
